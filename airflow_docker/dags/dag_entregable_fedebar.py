0544926007de730a6e47fca1dd30edbc5790e822
fedebaer <fedebaer@gmail.com>
2024-06-05 21:57:28 -0300

    entrega final

--- airflow_docker/dags/dag_entregable_fedebar.py
+++ airflow_docker/dags/dag_entregable_fedebar.py
@@ -1,21 +1,27 @@
-from datetime import timedelta,datetime
+from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
-from weather_info_ETL import  insertar_data, traer_datos_clima,crear_tabla_en_bd
+import logging
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from airflow.hooks.base_hook import BaseHook
+from weather_info_ETL import enviar_email, traer_datos_clima, crear_tabla_en_bd, insertar_data
 
 default_args = {
     'owner': 'FedeBar',
     'start_date': datetime(2024, 5, 27),
     'retries': 1,
-    'retry_delay': timedelta(minutes=3)
+    'retry_delay': timedelta(minutes=3),
+    'on_failure_callback': enviar_email  # Callback para enviar email en caso de fallo
 }
 
 ingestion_dag = DAG(
     dag_id='ingestion_data',
     default_args=default_args,
     description='Agrega datos de clima de cada día',
-     schedule_interval=timedelta(days=1),
+    schedule_interval=timedelta(days=1),
     catchup=False
 )
 
@@ -24,7 +30,6 @@ task_1 = PythonOperator(
     python_callable=traer_datos_clima,
     dag=ingestion_dag,
 )
-
 
 task_2 = PythonOperator(
     task_id='crear_tabla_si_no_existe',
@@ -38,8 +43,11 @@ task_3 = PythonOperator(
     dag=ingestion_dag,
 )
 
+task_4 = PythonOperator(
+    task_id='enviar_notificacion',
+    python_callable=enviar_email,
+    provide_context=True,  
+    dag=ingestion_dag,
+)
 
-
-task_1 >> task_2 >> task_3 
-
-
+task_1 >> task_2 >> task_3 >> task_4
--- airflow_docker/dags/weather_info_ETL.py
+++ airflow_docker/dags/weather_info_ETL.py
@@ -1,14 +1,21 @@
-import openmeteo_requests 
-import requests_cache 
-from retry_requests import retry
-from airflow.models import DAG, Variable
-from airflow.operators.python import PythonOperator
-from airflow.hooks.base_hook import BaseHook
+import logging
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from datetime import datetime, timedelta
+
 import pandas as pd
 import psycopg2
 from psycopg2.extras import execute_values
-import logging
-from datetime import datetime
+from airflow import DAG
+from airflow.hooks.base_hook import BaseHook
+from airflow.operators.python import PythonOperator
+
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+
+
 
 def conectar_redshift():
     try:
@@ -16,16 +23,18 @@ def conectar_redshift():
         logging.info(f"Conectando a Redshift en {connection.host}:{connection.port}/{connection.schema}")
        
         conn = psycopg2.connect(
-            host = connection.host,
-            dbname = connection.schema,
-            user = connection.login,
-            password = connection.password,
-            port = connection.port
+            host=connection.host,
+            dbname=connection.schema,
+            user=connection.login,
+            password=connection.password,
+            port=connection.port
         )
-        return conn
         logging.info("Conectado a Redshift con éxito!")
+        return conn
     except Exception as e:
-        logging.error(f"No es posible conectar a Redshift: {str(e)}")
+        error_message = f"No es posible conectar a Redshift: {str(e)}"
+        logging.error(error_message)
+        raise RuntimeError(error_message)
 
 def crear_tabla_en_bd():
     conn = conectar_redshift()
@@ -54,6 +63,7 @@ def crear_tabla_en_bd():
                         longitude FLOAT NULL,
                         timezone VARCHAR(50) NULL,
                         city VARCHAR(50) NULL,
+                        habitantes BIGINT,
                         load_date DATE NULL,
                         PRIMARY KEY (m_date, load_date, city)
                     )
@@ -61,86 +71,89 @@ def crear_tabla_en_bd():
                 conn.commit()
                 logging.info("Tabla creada con éxito en Redshift.")
         except Exception as e:
-            logging.error(f"No es posible crear tabla en Redshift: {str(e)}")
+            error_message = f"No es posible crear tabla en Redshift: {str(e)}"
+            logging.error(error_message)
+            raise RuntimeError(error_message)
         finally:
             conn.close()
             logging.info("Conexión a Redshift cerrada.")
 
-    
 def traer_datos_clima():
-     # Setup the Open-Meteo API client with cache and retry on error
-    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-    openmeteo = openmeteo_requests.Client(session = retry_session)
+    try:
+        # Setup the Open-Meteo API client with cache and retry on error
+        cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+        retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+        openmeteo = openmeteo_requests.Client(session=retry_session)
 
-    # weather variables list. Se agrega el nombre de las ciudades que corresponden a cada par de latitud-longitud
-    url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-        "latitude": [-32.8908, -34.6131, -24.7859, -31.4135, -33.295, -26.8241, -31.6488],
-        "longitude": [-68.8272, -58.3772, -65.4117, -64.181, -66.3356, -65.2226, -60.7087],
-        "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "apparent_temperature_max", "apparent_temperature_min", "sunrise", "sunset", "daylight_duration", "sunshine_duration", "precipitation_sum", "rain_sum", "showers_sum", "snowfall_sum", "precipitation_hours", "precipitation_probability_max"],
-        "timezone": ["America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo"],
-        "past_days": 7,
-        "elevation": ["NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"],
-        "city": ["Mendoza", "Buenos Aires", "Salta", "Cordoba", "San Luis", "Tucuman", "Santa Fe"]
-
-    }
-    responses = openmeteo.weather_api(url, params=params)
-
-    completed_dataframe = pd.DataFrame()
-
-    # Se crea un bucle para procesar cada localidad
-    for index, response in enumerate(responses):
-        latitude = params["latitude"][index]
-        longitude = params["longitude"][index]
-        timezone = params["timezone"][index]
-        elevation = params["elevation"][index]
-        city = params["city"][index]
-
-
-        daily = response.Daily()
-
-        # Se crea un diccionario para cada Provincia con la información diaria de pronósticos.
-        daily_data = {
-            "m_date": pd.date_range(
-                start=pd.to_datetime(daily.Time(), unit="s", utc=True),
-                end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
-                freq=pd.Timedelta(seconds=daily.Interval()),
-                inclusive="left",
-            ).date,
-            "weather_code": daily.Variables(0).ValuesAsNumpy(),
-            "temperature_2m_max": daily.Variables(1).ValuesAsNumpy(),
-            "temperature_2m_min": daily.Variables(2).ValuesAsNumpy(),
-            "apparent_temperature_max": daily.Variables(3).ValuesAsNumpy(),
-            "apparent_temperature_min": daily.Variables(4).ValuesAsNumpy(),
-            "sunrise": daily.Variables(5).ValuesAsNumpy(),
-            "sunset": daily.Variables(6).ValuesAsNumpy(),
-            "daylight_duration": daily.Variables(7).ValuesAsNumpy(),
-            "sunshine_duration": daily.Variables(8).ValuesAsNumpy(),
-            "precipitation_sum": daily.Variables(9).ValuesAsNumpy(),
-            "rain_sum": daily.Variables(10).ValuesAsNumpy(),
-            "showers_sum": daily.Variables(11).ValuesAsNumpy(),
-            "snowfall_sum": daily.Variables(12).ValuesAsNumpy(),
-            "precipitation_hours": daily.Variables(13).ValuesAsNumpy(),
-            "precipitation_probability_max": daily.Variables(14).ValuesAsNumpy(),
+        # weather variables list. Se agrega el nombre de las ciudades que corresponden a cada par de latitud-longitud
+        url = "https://api.open-meteo.com/v1/forecast"
+        params = {
+            "latitude": [-32.8908, -34.6131, -24.7859, -31.4135, -33.295, -26.8241, -31.6488],
+            "longitude": [-68.8272, -58.3772, -65.4117, -64.181, -66.3356, -65.2226, -60.7087],
+            "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "apparent_temperature_max", "apparent_temperature_min", "sunrise", "sunset", "daylight_duration", "sunshine_duration", "precipitation_sum", "rain_sum", "showers_sum", "snowfall_sum", "precipitation_hours", "precipitation_probability_max"],
+            "timezone": ["America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo", "America/Sao_Paulo"],
+            "past_days": 7,
+            "elevation": ["NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"],
+            "city": ["Mendoza", "Buenos Aires", "Salta", "Cordoba", "San Luis", "Tucuman", "Santa Fe"]
         }
+        responses = openmeteo.weather_api(url, params=params)
 
-        # Se pasa esa info a un DataFrame
-        daily_dataframe = pd.DataFrame(data=daily_data)
+        completed_dataframe = pd.DataFrame()
 
-        # Se agregan nuevas columnas con los parámetros usados
-        daily_dataframe["latitude"] = latitude
-        daily_dataframe["longitude"] = longitude
-        daily_dataframe["timezone"] = timezone
-        daily_dataframe["city"] = city
-        daily_dataframe["load_date"] = pd.Timestamp(datetime.now()).date()
+        # Se crea un bucle para procesar cada localidad
+        for index, response in enumerate(responses):
+            latitude = params["latitude"][index]
+            longitude = params["longitude"][index]
+            timezone = params["timezone"][index]
+            elevation = params["elevation"][index]
+            city = params["city"][index]
 
+            daily = response.Daily()
 
-        # Se agrega esta info al dataframe general
-        completed_dataframe = pd.concat([completed_dataframe, daily_dataframe], ignore_index=True)
+            # Se crea un diccionario para cada Provincia con la información diaria de pronósticos.
+            daily_data = {
+                "m_date": pd.date_range(
+                    start=pd.to_datetime(daily.Time(), unit="s", utc=True),
+                    end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
+                    freq=pd.Timedelta(seconds=daily.Interval()),
+                    inclusive="left",
+                ).date,
+                "weather_code": daily.Variables(0).ValuesAsNumpy(),
+                "temperature_2m_max": daily.Variables(1).ValuesAsNumpy(),
+                "temperature_2m_min": daily.Variables(2).ValuesAsNumpy(),
+                "apparent_temperature_max": daily.Variables(3).ValuesAsNumpy(),
+                "apparent_temperature_min": daily.Variables(4).ValuesAsNumpy(),
+                "sunrise": daily.Variables(5).ValuesAsNumpy(),
+                "sunset": daily.Variables(6).ValuesAsNumpy(),
+                "daylight_duration": daily.Variables(7).ValuesAsNumpy(),
+                "sunshine_duration": daily.Variables(8).ValuesAsNumpy(),
+                "precipitation_sum": daily.Variables(9).ValuesAsNumpy(),
+                "rain_sum": daily.Variables(10).ValuesAsNumpy(),
+                "showers_sum": daily.Variables(11).ValuesAsNumpy(),
+                "snowfall_sum": daily.Variables(12).ValuesAsNumpy(),
+                "precipitation_hours": daily.Variables(13).ValuesAsNumpy(),
+                "precipitation_probability_max": daily.Variables(14).ValuesAsNumpy(),
+            }
 
-    completed_dataframe.sort_values(by=['city', 'm_date'], ascending=False)
-    return(completed_dataframe)
+            # Se pasa esa info a un DataFrame
+            daily_dataframe = pd.DataFrame(data=daily_data)
+
+            # Se agregan nuevas columnas con los parámetros usados
+            daily_dataframe["latitude"] = latitude
+            daily_dataframe["longitude"] = longitude
+            daily_dataframe["timezone"] = timezone
+            daily_dataframe["city"] = city
+            daily_dataframe["load_date"] = pd.Timestamp(datetime.now()).date()
+
+            # Se agrega esta info al dataframe general
+            completed_dataframe = pd.concat([completed_dataframe, daily_dataframe], ignore_index=True)
+
+        completed_dataframe.sort_values(by=['city', 'm_date'], ascending=False)
+        return completed_dataframe
+    except Exception as e:
+        error_message = f"Error al traer datos del clima: {str(e)}"
+        logging.error(error_message)
+        raise RuntimeError(error_message)
 
 def insertar_data():
     conn = conectar_redshift()
@@ -165,20 +178,61 @@ def insertar_data():
                             apparent_temperature_max, apparent_temperature_min, sunrise, sunset,
                             daylight_duration, sunshine_duration, precipitation_sum, rain_sum,
                             showers_sum, snowfall_sum, precipitation_hours, precipitation_probability_max,
-                            latitude, longitude, timezone, city, load_date
+                            latitude, longitude, timezone, city,  load_date
                         ) VALUES %s
                         ''',
-                        [tuple(row) for row in completed_dataframe.values]
+                        [tuple(row) for row in completed_dataframe.itertuples(index=False, name=None)]
                     )
                     conn.commit()
                     logging.info("Datos insertados con éxito en la tabla daily_weather_3.")
             else:
                 logging.info("No hay nueva data para insertar.")
         except Exception as e:
-            logging.error(f"Error al insertar datos en Redshift: {str(e)}")
+            error_message = f"Error al insertar datos en Redshift: {str(e)}"
+            logging.error(error_message)
+            raise RuntimeError(error_message)
         finally:
             conn.close()
             logging.info("Conexión a Redshift cerrada.")
     else:
-        logging.error("Conexión a Redshift fallida, no se pueden insertar datos.")
+        error_message = "Conexión a Redshift fallida, no se pueden insertar datos."
+        logging.error(error_message)
+        raise RuntimeError(error_message)
 
+
+def enviar_email(**kwargs):
+    try:
+        context = kwargs.get('context', {})
+        connection = BaseHook.get_connection("mail_fede")
+        Pass_Email = connection.password
+        smtp_server = 'smtp.gmail.com'
+        smtp_port = 587
+        sender_email = connection.login
+        password = Pass_Email
+
+        subject = 'Carga de datos'
+        body_text = 'El proceso diario de carga de datos de clima terminó correctamente.'
+
+        if 'ti' in context and context['ti'].xcom_pull(task_ids='insertar_datos', key='return_value'):
+            body_text += '\n\nSe encontraron los siguientes errores:\n' + context['ti'].xcom_pull(task_ids='insertar_datos', key='return_value')
+
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = sender_email
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body_text, 'plain'))
+
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(sender_email, password)
+            server.send_message(msg)
+        logging.info('El email fue enviado correctamente.')
+
+    except smtplib.SMTPAuthenticationError as auth_error:
+        error_message = f"Error de autenticación al enviar el email: {auth_error}"
+        logging.error(error_message)
+        raise RuntimeError(error_message)
+    except Exception as exception:
+        error_message = f"Error al enviar el email: {exception}"
+        logging.error(error_message)
+        raise RuntimeError(error_message)
